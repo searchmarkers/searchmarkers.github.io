4104909f4232779b373a572b6fa7bba295c3a680
searchmarkers <dev@example.com>
2022-10-26 15:00:01 +0000

    Update dist

--- dist/manage-storage.js
+++ dist/manage-storage.js
@@ -1,3 +1,46 @@
+
+window["chrome"] = {
+    runtime: {
+        getManifest: () => ({
+            name: "Mark My Search",
+            version: "X.Y.Z",
+        }),
+    },
+    storage: {
+        session: {
+            set: () => undefined,
+            get: () => {
+                const store = {};
+                Object.keys(StorageSession).forEach(key => store[key] = {});
+                return store;
+            },
+        },
+        local: {
+            set: () => undefined,
+            get: () => {
+                const store = {};
+                Object.keys(StorageLocal).forEach(key => store[key] = {});
+                return store;
+            },
+        },
+        sync: {
+            set: () => undefined,
+            get: () => defaultOptions,
+        },
+        onChanged: {
+            addListener: () => undefined,
+        },
+    },
+    tabs: {
+        query: async () => ([ {
+            id: -1,
+            url: window.location.href,
+        } ]),
+        onActivated: {
+            addListener: () => undefined,
+        },
+    },
+};
 "use strict";
 var _a;
 var _b;
