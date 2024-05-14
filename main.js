b1ca934b2d62200ac1236eea86fd7684513b3203
ator-dev <dominic.codedeveloper@gmail.com>
2024-05-14 16:11:33 +0100

    Introduce JS to store and retrieve theme value

---
+++ main.js
@@ -0,0 +1,22 @@
+addEventListener("load", () => {
+	const theme = localStorage.getItem("theme");
+	if (theme !== null) {
+		const checkbox = document.getElementById("theme-" + theme);
+		if (checkbox) {
+			checkbox.checked = true;
+		} else {
+			console.warn("The stored theme '" + theme + "' is not available on this page. Selecting automatic theme.");
+		}
+	}
+	for (const checkbox of document.querySelectorAll("input[name='theme']")) {
+		checkbox.addEventListener("change", () => {
+			if (checkbox.checked) {
+				if (checkbox.id === "theme") {
+					localStorage.removeItem("theme");
+				} else {
+					localStorage.setItem("theme", checkbox.id.slice(checkbox.id.indexOf("-") + 1));
+				}
+			}
+		});
+	}
+});
