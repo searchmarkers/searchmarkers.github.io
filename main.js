6780e5e8d3f27da68b541e6101c02aa98e648078
ator-dev <dominic.codedeveloper@gmail.com>
2025-04-08 19:24:39 +0100

    Improve main script

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const theme = localStorage.getItem("theme");
-	if (theme !== null) {
-		const input = document.getElementById("theme-" + theme);
-		if (input) {
-			input.checked = true;
-		} else {
-			console.warn("The stored theme '" + theme + "' is not available on this page. Using automatic theme.");
-		}
+const loadTheme = theme => {
+	const input = document.getElementById("theme-" + theme);
+	if (input) {
+		input.checked = true;
+	} else {
+		return false;
 	}
-}, { passive: true });
+};
+
+const storedTheme = localStorage.getItem("theme");
+
+if (storedTheme !== null && !loadTheme(storedTheme)) {
+	document.addEventListener("DOMContentLoaded", () => {
+		if (!loadTheme(theme)) {
+			console.warn("Theme '" + theme + "' is not available on this page. Using automatic theme.");
+		}
+	}, { passive: true });
+}
 
 addEventListener("input", event => {
 	const input = event.target;
